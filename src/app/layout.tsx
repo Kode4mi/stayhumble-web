591183f3dbb5a784719f0f270bd2b859423e3f05
qspyx <mikolajszokaluk@gmail.com>
2023-12-13 18:41:19 +0100

    page title typo fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'staHumble',
+  title: 'stayHumble',
   description: 'Created by kode4mi',
 }
 
