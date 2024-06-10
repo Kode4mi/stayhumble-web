35c434d37274b80feba41b3266facf2e3efc71e7
Mikołaj Szokaluk <mikolajszokaluk@gmail.com>
2024-06-10 11:16:46 +0200

    header and main page changes and navbar implementation

--- src/app/header/page.tsx
+++ src/app/header/page.tsx
@@ -3,18 +3,19 @@ import Header from "@/components/molecules/Header";
 import SuggestedFriendsNavbar from "@/components/molecules/SuggestedFriendsNavbar";
 import SearchBar from "@/components/atoms/SearchBar";
 import Post from "@/components/molecules/Post";
+import Navbar from "@/components/molecules/Navbar";
 
 export default function HeaderPage() {
   return (
     <div className="flex flex-row">
-      <div className="w-[22%]">
-        Navbar
+      <div className="w-[24%] h-screen">
+        <Navbar />
       </div>
-      <div className="w-[56%]">
+      <div className="w-[52%]">
         <Header />
           <Post authorName={"KotKamil"} content={"CIAPAKAI"} likes={123} dislikes={2} shares={2} comments={2} />
       </div>
-      <div className="w-[22%]">
+      <div className="w-[24%]">
         <SearchBar />
         <SuggestedFriendsNavbar />
       </div>
---
+++ src/components/molecules/Navbar.tsx
@@ -0,0 +1,22 @@
+"use client";
+import ColoredButton from "../atoms/commons/ColoredButton";
+
+export default function Navbar() {
+  return (
+    <div className="flex flex-col items-center bg-slate-100 dark:bg-black p-4 h-full">
+      <h1 className="text-[4rem] font-bold pt-6 pb-8 text-center transition-all hover:cursor-default hover:scale-105">
+        <span className="text-black dark:text-white">stay</span>
+        <span className="text-orange-500">Humble</span>
+      </h1>
+      <hr className="w-[90%] pb-4 border-slate-300 dark:border-white " />
+      <div className="flex flex-col items-center space-y-2 text-3xl">
+        <div className="flex items-center space-x-2">
+          <ColoredButton content="Main Page" background="none" onClick={null} size="custom" icon="home" additionalStyles="text-4xl rounded-lg py-8" />
+        </div>
+        <ColoredButton content="Search" background="none" onClick={null} size="custom" icon="search" additionalStyles="text-4xl rounded-lg py-8" />
+        <ColoredButton content="Notifications" background="none" onClick={null} size="custom" icon="notifications" additionalStyles="text-4xl rounded-lg py-8" />
+        <ColoredButton content="Messages" background="none" onClick={null} size="custom" icon="chat" additionalStyles="text-4xl rounded-lg py-8" />
+      </div>
+    </div>
+  );
+}
