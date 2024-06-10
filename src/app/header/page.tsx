8e59cee3584da2046940664e78af9ff34cbaaf50
Mikołaj Szokaluk <mikolajszokaluk@gmail.com>
2024-06-10 12:53:17 +0200

    user info in navbar

--- src/app/header/page.tsx
+++ src/app/header/page.tsx
@@ -8,10 +8,11 @@ import Navbar from "@/components/molecules/Navbar";
 export default function HeaderPage() {
   return (
     <div className="flex flex-row bg-slate-100 dark:bg-gray-900">
-      <div className="w-[24%] h-screen">
+      {/* 24, 52, 24??? */}
+      <div className="w-[22%] h-screen"> 
         <Navbar />
       </div>
-      <div className="w-[52%]">
+      <div className="w-[56%]">
         <Header />
         <div className="px-4 space-y-8 pt-4">
           <Post authorName={"KotKamil"} content={"CIAPAKAI"} likes={123} dislikes={2} shares={2} comments={2} />
@@ -19,7 +20,7 @@ export default function HeaderPage() {
           <Post authorName={"KotKamil"} content={"CIAPAKAI"} likes={123} dislikes={2} shares={2} comments={2} />
         </div>
       </div>
-      <div className="w-[24%]">
+      <div className="w-[22%]"> 
         <SearchBar />
         <SuggestedFriendsNavbar />
       </div>
--- src/components/molecules/Navbar.tsx
+++ src/components/molecules/Navbar.tsx
@@ -1,21 +1,34 @@
 "use client";
+import Link from "next/link";
 import ColoredButton from "../atoms/commons/ColoredButton";
+import AuthorInfo from "../atoms/AuthorInfo";
 
 export default function Navbar() {
   return (
-    <div className="flex flex-col items-center bg-slate-200 dark:bg-[#090d15] p-4 h-full">
-      <h1 className="text-[4rem] font-bold pt-6 pb-8 text-center transition-all hover:cursor-default hover:scale-105">
-        <span className="text-black dark:text-white">stay</span>
-        <span className="text-orange-500">Humble</span>
-      </h1>
-      <hr className="w-[90%] pb-4 border-slate-300 dark:border-white " />
-      <div className="flex flex-col items-center space-y-2 text-3xl">
-        <div className="flex items-center space-x-2">
-          <ColoredButton content="Main Page" background="none" size="custom" icon="home" additionalStyles="text-4xl rounded-lg py-8" />
+    <div className="flex flex-col items-center justify-between bg-slate-200 dark:bg-[#090d15] p-4 h-full">
+      <div>
+        <Link href="/header">
+          <h1 className="text-[4rem] font-bold pt-6 pb-8 text-center transition-all hover:scale-105">
+            <span className="text-black dark:text-white">stay</span>
+            <span className="text-orange-500">Humble</span>
+          </h1>
+        </Link>
+        <div className="flex justify-center w-full">
+          <hr className="w-[90%] border-slate-300 dark:border-white" />
         </div>
-        <ColoredButton content="Search" background="none" size="custom" icon="search" additionalStyles="text-4xl rounded-lg py-8" />
-        <ColoredButton content="Notifications" background="none" size="custom" icon="notifications" additionalStyles="text-4xl rounded-lg py-8" />
-        <ColoredButton content="Messages" background="none" size="custom" icon="chat" additionalStyles="text-4xl rounded-lg py-8" />
+        <div className="flex flex-col space-y-2 text-3xl py-4">
+          <Link href="/header">
+            <ColoredButton content="Main Page" background="none" size="custom" icon="home" additionalStyles="text-4xl rounded-lg py-8 w-64" />
+          </Link>
+          <ColoredButton content="Search" background="none" size="custom" icon="search" additionalStyles="text-4xl rounded-lg py-8 w-64" />
+          <ColoredButton content="Notifications" background="none" size="custom" icon="notifications" additionalStyles="text-4xl rounded-lg py-8 w-64" />
+          <ColoredButton content="Messages" background="none" size="custom" icon="chat" additionalStyles="text-4xl rounded-lg py-8 w-64" />
+        </div>
+      </div>
+      <div className="mb-16 scale-[1.75] p-4 w-48 border-gray-400 dark:border-black border rounded-md hover:scale-[1.8] hover:cursor-pointer">
+        <Link href="/users/1">
+          <AuthorInfo authorName="Kamil" />{" "}
+        </Link>
       </div>
     </div>
   );
