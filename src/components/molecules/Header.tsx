522960b2be59b94df794abd130528f10b7769842
Mikołaj Szokaluk <mikolajszokaluk@gmail.com>
2024-06-11 13:54:19 +0200

    orange accents

--- src/components/molecules/Header.tsx
+++ src/components/molecules/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <div className="flex items-center justify-between dark:bg-gray-900 text-xl pb-4">
       <button className="text-black font-bold relative px-4 py-7 w-1/2 transition-all hover:scale-105 dark:text-white dark:border-white ">
         <span>Dla Ciebie</span>
-        <span className="absolute bottom-0 mb-2 left-1/4 w-1/2 h-[1px] bg-slate-300 dark:bg-white"></span>
+        <span className="absolute bottom-0 mb-2 left-1/4 w-1/2 h-[1px] bg-orange-500"></span>
       </button>
       <button className="text-black font-bold relative px-4 py-7 w-1/2 transition-all hover:scale-105 dark:text-white dark:border-white ml-2 ">
         <span>Obserwujesz</span>
