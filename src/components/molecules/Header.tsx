75c9db0630adeb03f424192162e9eadd9b014a4d
KotKamil <62114754+KotKamil@users.noreply.github.com>
2024-06-12 18:08:02 +0200

    Updated home page design

--- src/components/molecules/Header.tsx
+++ src/components/molecules/Header.tsx
@@ -1,16 +1,19 @@
 "use client";
 
+import Link from "next/link";
+import {usePathname} from "next/navigation";
+
 export default function Header() {
-  return (
-    <div className="flex items-center justify-between dark:bg-gray-900 text-xl pb-4">
-      <button className="text-black font-bold relative px-4 py-7 w-1/2 transition-all hover:scale-105 dark:text-white dark:border-white ">
-        <span>Dla Ciebie</span>
-        <span className="absolute bottom-0 mb-2 left-1/4 w-1/2 h-[1px] bg-orange-500"></span>
-      </button>
-      <button className="text-black font-bold relative px-4 py-7 w-1/2 transition-all hover:scale-105 dark:text-white dark:border-white ml-2 ">
-        <span>Obserwujesz</span>
-        <span className="absolute bottom-0 mb-2 left-1/4 w-1/2 h-[1px] bg-slate-300 dark:bg-white"></span>
-      </button>
-    </div>
-  );
+    const pathname = usePathname();
+
+    return (
+        <div className="flex items-center justify-between dark:bg-gray-900 text-xl pb-4 text-orange-500">
+            <Link href="/home" className="text-black text-center font-bold relative px-4 py-7 w-1/2 dark:text-white dark:border-white">
+                <span className={pathname === "/home" ? "text-orange-500": ""}>Dla Ciebie</span>
+            </Link>
+            <Link href="/home/following" className="text-black text-center font-bold relative px-4 py-7 w-1/2 dark:text-white dark:border-white ml-2">
+                <span className={pathname === "/home/following" ? "text-orange-500": ""}>Obserwujesz</span>
+            </Link>
+        </div>
+    );
 }
