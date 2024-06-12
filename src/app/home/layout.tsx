360782a6118ba52e1989fb517d9aa0a8bada83fd
Mikołaj Szokaluk <mikolajszokaluk@gmail.com>
2024-06-12 17:27:30 +0200

    mobile display and statistics layout tweaks

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -3,16 +3,32 @@ import SuggestedFriendsNavbar from "@/components/molecules/SuggestedFriendsNavba
 import SearchBar from "@/components/atoms/SearchBar";
 import Navbar from "@/components/molecules/Navbar";
 
-export default function layout({children}: {children: React.ReactNode}) {
+export default function layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="grid grid-cols-[22%_1fr_22%] h-full min-h-screen w-screen bg-slate-100 dark:bg-gray-900 ">
-      <div className="h-screen left-0 top-0 bottom-0 web:flex hidden fixed  ">
+    <div className="grid web:grid-cols-[22%_1fr_22%] h-full min-h-screen w-screen web:overflow-x-auto overflow-x-hidden bg-slate-100 dark:bg-gray-900">
+      <div className="h-screen left-0 top-0 bottom-0 web:flex hidden fixed">
         <Navbar />
       </div>
       <div className="w-full col-start-2 col-end-3 overflow-y-auto h-full web:px-24">{children}</div>
-      <div className="h-screen fixed right-0 top-0 bottom-0 web:block hidden ">
+      <div className="h-screen fixed right-0 top-0 bottom-0 web:block hidden">
         <SearchBar />
         <SuggestedFriendsNavbar />
+      </div>
+      <div className="w-full fixed bottom-0 bg-transparent p-4 web:hidden ">
+        <div className="flex justify-around ">
+          <button className="flex flex-col items-center">
+            <span className="material-symbols-outlined text-orange-600">home</span>
+            <span>Dom</span>
+          </button>
+          <button className="flex flex-col items-center">
+            <span className="material-symbols-outlined text-orange-600">search</span>
+            <span>Szukaj</span>
+          </button>
+          <button className="flex flex-col items-center">
+            <span className="material-symbols-outlined text-orange-600">person</span>
+            <span>Profil</span>
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/components/atoms/PostStats.tsx
+++ src/components/atoms/PostStats.tsx
@@ -15,33 +15,33 @@ const PostStats: React.FC<PostStatsProps> = ({ likes, dislikes, comments, shares
     if (num < 1000000000) return (num / 1000000).toFixed(1) + "M";
     return (num / 1000000000).toFixed(1) + "B";
   };
-  
+
   const shortTotalLikes = shortenNumber(likes);
   const shortComments = shortenNumber(comments);
   const shortShares = shortenNumber(shares);
 
   return (
     <div className="w-full h-8 leading-8 font-light flex items-center">
-      <div className="flex items-center justify-between w-[12%]">
+      <div className="flex items-center justify-between web:w-[16%] w-1/4 ">
         <button className="flex items-center">
-          <span className="material-symbols-outlined text-orange-500">thumb_up</span>
+          <span className="material-symbols-outlined text-orange-500 text-2xl">thumb_up</span>
         </button>
         <span className="overflow-x-hidden">{shortTotalLikes}</span>
         <button className="flex items-center">
-          <span className="material-symbols-outlined">thumb_down</span>
+          <span className="material-symbols-outlined text-2xl">thumb_down</span>
         </button>
       </div>
-      <div className="flex items-center w-[13%]  ">
+      <div className="flex items-center web:w-[18%] w-1/4  ">
         <button className="flex items-center ml-8" onClick={handleComment}>
-          <span className="material-symbols-outlined">comment</span>
+          <span className="material-symbols-outlined text-2xl">comment</span>
         </button>
-        <span className="mx-4 overflow-x-hidden">{shortComments}</span>
+        <span className=" ml-2 overflow-x-hidden">{shortComments}</span>
       </div>
       <div className="flex items-center">
         <button className="flex items-center">
-          <span className="material-symbols-outlined">share</span>
+          <span className="material-symbols-outlined text-2xl">share</span>
         </button>
-        <span className="mx-4 overflow-x-hidden">{shortShares}</span>
+        <span className="ml-2 overflow-x-hidden">{shortShares}</span>
       </div>
     </div>
   );
