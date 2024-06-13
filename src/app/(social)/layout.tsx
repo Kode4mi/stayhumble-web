9e95900b79386a83277eeeb3b0a5d6e4ef3f911f
Mikołaj Szokaluk <mikolajszokaluk@gmail.com>
2024-06-13 21:01:41 +0200

    theme switch

--- src/app/(social)/layout.tsx
+++ src/app/(social)/layout.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import SuggestedFriendsNavbar from "@/components/molecules/SuggestedFriendsNavbar";
-import SearchBar from "@/components/atoms/SearchBar";
 import Navbar from "@/components/molecules/Navbar";
+import ThemeSwitch from "@/components/atoms/ThemeSwitch";
 
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="grid web:grid-cols-[22%_1fr_22%] h-full min-h-screen w-screen web:overflow-x-auto overflow-x-hidden bg-slate-100 dark:bg-gray-900">
+    <div className="grid web:grid-cols-[22%_1fr_22%] h-full min-h-screen w-screen web:overflow-x-auto overflow-x-hidden bg-slate-100 dark:bg-gray-900 text-black dark:text-white">
       <div className="h-screen left-0 top-0 bottom-0 web:flex hidden fixed">
         <Navbar />
       </div>
       <div className="w-full col-start-2 col-end-3 overflow-y-auto h-full web:px-24">{children}</div>
       <div className="h-screen fixed right-0 top-0 bottom-0 web:block hidden">
-        <SearchBar />
+        <div className="flex justify-end p-4 mr-8">
+        <ThemeSwitch />
+        </div>
         <SuggestedFriendsNavbar />
       </div>
       <div className="w-full fixed bottom-0 bg-transparent p-4 web:hidden ">
---
+++ src/components/atoms/ThemeSwitch.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
+import Image from "next/image";
+
+export default function ThemeSwitch() {
+  const [mounted, setMounted] = useState(false);
+  const { setTheme, resolvedTheme } = useTheme();
+
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted)
+    return (
+      <Image
+        src="data:image/svg+xml;base64,PHN2ZyBzdHJva2U9IiNGRkZGRkYiIGZpbGw9IiNGRkZGRkYiIHN0cm9rZS13aWR0aD0iMCIgdmlld0JveD0iMCAwIDI0IDI0IiBoZWlnaHQ9IjIwMHB4IiB3aWR0aD0iMjAwcHgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHJlY3Qgd2lkdGg9IjIwIiBoZWlnaHQ9IjIwIiB4PSIyIiB5PSIyIiBmaWxsPSJub25lIiBzdHJva2Utd2lkdGg9IjIiIHJ4PSIyIj48L3JlY3Q+PC9zdmc+Cg=="
+        width={36}
+        height={36}
+        sizes="36x36"
+        alt="Loading Light/Dark Toggle"
+        priority={false}
+        title="Loading Light/Dark Toggle"
+      />
+    );
+
+  if (resolvedTheme === "dark") {
+    return (
+      <span className="material-symbols-outlined" onClick={() => setTheme("light")}>
+        light_mode
+      </span>
+    );
+  }
+
+  if (resolvedTheme === "light") {
+    return (
+      <span className="material-symbols-outlined" onClick={() => setTheme("dark")}>
+        dark_mode
+      </span>
+    );
+  }
+}
