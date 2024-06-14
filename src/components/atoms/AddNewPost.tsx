6f8c2c9020a469e9a4443efc854720df732220bd
KotKamil <62114754+KotKamil@users.noreply.github.com>
2024-06-14 14:06:25 +0200

    Created simple search page

--- src/components/atoms/AddNewPost.tsx
+++ src/components/atoms/AddNewPost.tsx
@@ -26,7 +26,7 @@ export function AddNewPost() {
 
     return (
         <div className="dark:bg-gray-950 bg-slate-400 w-full min-h-48 rounded-xl px-6 py-4">
-            <textarea placeholder="Napisz co się stało..." className="bg-transparent w-full outline-0 resize-none h-28"/>
+            <textarea placeholder="Napisz co się stało..." className="bg-transparent w-full outline-0 resize-none h-28 placeholder-gray-950 dark:placeholder-gray-400"/>
             {selectedImageURL &&
                 <div className="w-20 h-20 rounded-2xl overflow-hidden relative mx-2 mb-2 cursor-pointer" onClick={cancelImageSelect}>
                         <span className="absolute text-8xl leading-[5rem] text-center w-20 bg-opacity-50 bg-black">
